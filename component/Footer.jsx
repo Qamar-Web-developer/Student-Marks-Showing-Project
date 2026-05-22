d10bc4fadc94ede4ba03d4798b71a5d4878628cb
Qamar Abbas <qamarwebsitedeveloper@gmail.com>
2026-05-22 09:58:02 +0500

    change email

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -3,24 +3,14 @@ import "../Footer.css";
 
 export default function Footer() {
   const phoneNumber = "923408286260";
-  const email = "qamarwebsitedeveloper@gmail.com";
+  const email = "ganishaambrik@gmail.com";
 
   return (
     <footer className="footer">
       <div className="footer-container">
         <h3>Contact Me</h3>
 
-        <p>
-          📱 WhatsApp:{}
-          <a
-            href={`https://wa.me/${923408286260}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-link"
-          >
-            +{phoneNumber}
-          </a>
-        </p>
+       
 
         <p>
           📧 Email:{" "}
